11ac41d6b64b97d5bc5cf79b5652e041c935d326
Ayumi Sato <sato@necco.inc>
2024-12-25 11:01:33 +0900

    [add] エラー発生時に、エラーの内容をフロントにも表示

--- src/components/useToast.tsx
+++ src/components/useToast.tsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 const useToast = () => {
   const [toast, setToast] = useState({ message: "", visible: false });
 
-  const showToast = (message: string) => {
+  const showToast = (message: string, options?: { type: string }) => {
     setToast({ message, visible: true });
-    setTimeout(() => {
-      setToast({ message: "", visible: false });
-    }, 3000);
   };
 
-  return { toast, showToast };
+  const hideToast = () => {
+    setToast({ message: "", visible: false });
+  };
+
+  return { toast, showToast, hideToast };
 };
 
 export default useToast;
